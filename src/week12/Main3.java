ecd5e028344489390551d574a4b9bab399b2b524
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-07-09 23:37:28 +0900

    wip: 🚧 (week12) 課題12-2をやった！

--- src/week12/Main3.java
+++ src/week12/Main3.java
@@ -1,8 +1,23 @@
 package week12;
 
-//ここに自分の名前
 public class Main3 {
   public static void main(String[] args) {
+    // Observerを作る
+    ObserverView1 v1 = new ObserverView1();
+    ObserverView2 v2 = new ObserverView2();
+    ObserverView3 v3 = new ObserverView3();
+    // Subjectを作る
+    SubjectModel m = new SubjectModel();
 
+    // ObserverからSubjectを参照する
+    v1.setSubjectModel(m);
+    v2.setSubjectModel(m);
+    v3.setSubjectModel(m);
+    // SubjectにObserverを追加する
+    m.addObserver(v1);
+    m.addObserver(v2);
+    m.addObserver(v3);
+    // 各Observerを更新する
+    m.notifyObserver();
   }
 }
