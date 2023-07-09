dc4002d9a6b82e2a08c03e5be068eab077a636ff
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-07-09 23:32:41 +0900

    feat: ✨ (week12) 課題12-3をやった！

--- src/week12/Main4.java
+++ src/week12/Main4.java
@@ -3,6 +3,22 @@ package week12;
 //ここに自分の名前
 public class Main4 {
   public static void main(String[] args) {
+    // Create instances of Observer
+    ObserverBarGraph observerBarGraph = new ObserverBarGraph();
+    ObserverLineGraph observerLineGraph = new ObserverLineGraph();
 
+    // Create instances of Subject
+    SubjectData subjectData = new SubjectData();
+
+    // Reference SubjectData from Observer
+    observerBarGraph.setSubjectData(subjectData);
+    observerLineGraph.setSubjectData(subjectData);
+
+    // Add Observers to SubjectData
+    subjectData.addObserver(observerBarGraph);
+    subjectData.addObserver(observerLineGraph);
+
+    // Update Observers
+    subjectData.notifyObserver();
   }
 }
